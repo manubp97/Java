ec71f09e088ff837dc2ff7a50834ee3351153e46
Manolo <manubp97@gmail.com>
2020-10-13 15:23:20 +0200

    Actualización 2

--- src/main/java/org/uma/mbd/mdNPI/Main.java
+++ src/main/java/org/uma/mbd/mdNPI/Main.java
@@ -5,20 +5,33 @@ import org.uma.mbd.mdNPI.npi.NPI;
 
 public class Main {
 	public static void main(String [] args) {
-		// 3 * (6 - 2) + (2 + 7) / 5;
-		// NPI 3 6 2 - * 2 7 + 5 / +
+		// 5 + (6 - 2) * 3;
+		// NPI 5 6 2 - 3 * +
 		NPI cpi = new NPI();
-		cpi.entra(3);
+		cpi.entra(5);
 		cpi.entra(6);
 		cpi.entra(2);
 		cpi.resta();
+		cpi.entra(3);
 		cpi.multiplica();
-		cpi.entra(2);
-		cpi.entra(7);
-		cpi.suma();
-		cpi.entra(5);
-		cpi.divide();
 		cpi.suma();
 		System.out.println("Resultado " + cpi.getResultado());
+
+		// 3 * (6 - 2) + (2 + 7) / 5;
+		// NPI 3 6 2 - * 2 7 + 5 / +
+		NPI cpi2 = new NPI();
+		cpi2.entra(3);
+		cpi2.entra(6);
+		cpi2.entra(2);
+		cpi2.resta();
+		cpi2.multiplica();
+		cpi2.entra(2);
+		cpi2.entra(7);
+		cpi2.suma();
+		cpi2.entra(5);
+		cpi2.divide();
+		cpi2.suma();
+		System.out.println("Resultado " + cpi2.getResultado());
+
 	}
 }
